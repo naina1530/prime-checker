442ac2d4ae96129ef9a1c4f363c4697c6033ec48
naina1530 <nainakhare.789@gmail.com>
2026-03-01 13:17:15 +0530

    initial commit

---
+++ prmine-checker.py
@@ -0,0 +1,11 @@
+def check_prime(num):
+    if num <= 1:
+        return "Not Prime"
+    for i in range(2, num):
+        if num % i == 0:
+            return "Not Prime"
+    return "Prime"
+
+number = int(input("Enter a number: "))
+result = check_prime(number)
+print("The number is:", result)
